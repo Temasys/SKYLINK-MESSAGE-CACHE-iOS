7c9c01110d0c344314b71d941ac01c393f86fa21
Lakindu Boteju <lakinduboteju@gmail.com>
2022-07-29 22:23:11 +0530

    Version 1.0.0

---
+++ SKYLINK_MESSAGE_CACHE.framework/Headers/MyTestClass.h
@@ -0,0 +1,18 @@
+//
+//  MyTestClass.h
+//  SKYLINK-MESSAGE-CACHE
+//
+//  Created by Lakindu Boteju on 2022-07-29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyTestClass : NSObject
+
+- (NSString *) getHello;
+
+@end
+
+NS_ASSUME_NONNULL_END
