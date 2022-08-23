bace1f6cae13f24cbd65233e8bd8de2b74b4fe19
Lakindu Boteju <lakinduboteju@gmail.com>
2022-08-24 01:43:13 +0530

    Bump-up the release version to 1.0.1.

--- SKYLINK_MESSAGE_CACHE.framework/Headers/SKYLINK_MESSAGE_CACHE.h
+++ SKYLINK_MESSAGE_CACHE.framework/Headers/SKYLINK_MESSAGE_CACHE.h
@@ -8,10 +8,10 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for SKYLINK_MESSAGE_CACHE.
-FOUNDATION_EXPORT double SKYLINK_MESSAGE_CACHEVersionNumber = 2;
+FOUNDATION_EXPORT double SKYLINK_MESSAGE_CACHEVersionNumber = 3;
 
 //! Project version string for SKYLINK_MESSAGE_CACHE.
-FOUNDATION_EXPORT const unsigned char SKYLINK_MESSAGE_CACHEVersionString[] = "1.0.1-beta";
+FOUNDATION_EXPORT const unsigned char SKYLINK_MESSAGE_CACHEVersionString[] = "1.0.1";
 
 // In this header, you should import all the public headers of your framework using statements like #import <SKYLINK_MESSAGE_CACHE/PublicHeader.h>
 #import "SkylinkMessageCache.h"
