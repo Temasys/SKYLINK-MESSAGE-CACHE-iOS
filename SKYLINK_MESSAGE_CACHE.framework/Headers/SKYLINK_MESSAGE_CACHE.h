3952582c009f38967d439b2435d59cab27676052
Lakindu Boteju <lakinduboteju@gmail.com>
2022-08-26 11:41:07 +0530

    Version 1.0.2

--- SKYLINK_MESSAGE_CACHE.framework/Headers/SKYLINK_MESSAGE_CACHE.h
+++ SKYLINK_MESSAGE_CACHE.framework/Headers/SKYLINK_MESSAGE_CACHE.h
@@ -8,10 +8,10 @@
 #import <Foundation/Foundation.h>
 
 //! Project version number for SKYLINK_MESSAGE_CACHE.
-FOUNDATION_EXPORT double SKYLINK_MESSAGE_CACHEVersionNumber = 3;
+FOUNDATION_EXPORT double SKYLINK_MESSAGE_CACHEVersionNumber = 4;
 
 //! Project version string for SKYLINK_MESSAGE_CACHE.
-FOUNDATION_EXPORT const unsigned char SKYLINK_MESSAGE_CACHEVersionString[] = "1.0.1";
+FOUNDATION_EXPORT const unsigned char SKYLINK_MESSAGE_CACHEVersionString[] = "1.0.2";
 
 // In this header, you should import all the public headers of your framework using statements like #import <SKYLINK_MESSAGE_CACHE/PublicHeader.h>
 #import "SkylinkMessageCache.h"
--- SKYLINK_MESSAGE_CACHE.podspec
+++ SKYLINK_MESSAGE_CACHE.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'SKYLINK_MESSAGE_CACHE'
-  s.version          = '1.0.1'
+  s.version          = '1.0.2'
   s.summary          = 'SKYLINK_MESSAGE_CACHE.framework distribution.'
 
 # This description is used to generate tags and improve search results.
